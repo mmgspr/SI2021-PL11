fe8deca88b3ebcf6927994679547896509db5e78
Manu <uo275943@uniovi.es>
2022-02-26 20:36:15 +0100

    archivos sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,24 +1,12 @@
-CREATE SCHEMA IF NOT EXISTS db_test; 
+DELETE FROM socios;
+DELETE FROM instalaciones;
+DELETE FROM reservas;
+DELETE FROM clientes;
+DELETE FROM actividades;
+DELETE FROM periodos_inscripcion;
+DELETE FROM inscripciones;
 
-  
-
-  
-
-CREATE TABLE  db_test.socios( 
-
-id_socio integer unsigned auto_increment primary key, 
-
-    nombre varchar(50), 
-
-    fecha_nacimiento date, 
-
-    dni varchar(9) 
-
-); 
-
-  
-
-INSERT INTO db_test.socios(nombre, fecha_nacimiento, dni) VALUES 
+INSERT INTO socios(nombre, fecha_nacimiento, dni) VALUES 
 
 ('David', '2001-12-21' , '12345678C'), 
 
@@ -26,163 +14,42 @@ INSERT INTO db_test.socios(nombre, fecha_nacimiento, dni) VALUES
 
 ('Juan', '1972-7-15', '13254768D'); 
 
-  
-
-CREATE TABLE db_test.cliente( 
-
-id_dni_cliente varchar(9) unique primary key, 
-
-    nombre varchar(50), 
-
-    fecha_nacimiento date 
-
-); 
-
-  
-
-INSERT INTO db_test.cliente(nombre, fecha_nacimiento, id_dni_cliente) VALUES 
+INSERT INTO clientes(nombre, fecha_nacimiento, id_dni_cliente) VALUES 
 
 ('Alvaro', '2001-12-21' , '18275678C'), 
 
 ('Daniel', '2005-6-24', '99654321F'), 
 
 ('Roberto', '1972-7-15', '21654768D'); 
-
-  
-
-  
-
-CREATE TABLE db_test.instalacion( 
-
-id_instalacion integer unsigned auto_increment primary key, 
-
-    nombre varchar(50), 
-
-    deporte varchar(20), 
-
-    precio decimal(10,2) 
-
-   ); 
-
-  
-
-INSERT INTO db_test.instalacion(nombre, deporte, precio) VALUES 
+INSERT INTO instalaciones(nombre, deporte, precio) VALUES 
 
 ('Pista de tenis 1', 'tenis', 7), 
 
 ('Pista de tenis 2', 'tenis', 5.3), 
 
 ('Pista de padel 1', 'padel', 10.50); 
-
-  
-
-  
-
-CREATE TABLE db_test.reserva( 
-
-id_reserva integer unsigned auto_increment primary key, 
-
-    foreign key (socio) references socios(id_socio), 
-
-    foreign key (instalacion) references instalacion(id_instalacion), 
-
-    fecha datetime, 
-
-    fecha_reserva datetime 
-
-   ); 
-
-  
-
-INSERT INTO db_test.reserva(socio, instalacion, fecha, fecha_reserva) VALUES 
+INSERT INTO reservas(socio, instalacion, fecha, fecha_reserva) VALUES 
 
 (1, 1, '2022-2-23 17:15:10', '2022-2-24 17:00:00'), 
 
 (2, 2,'2022-2-25 18:50:41', '2022-2-27 20:00:00'), 
 
 (3, 3,'2022-2-26 20:05:23', '2022-2-27 19:00:00'); 
-
- 
-
-CREATE TABLE db_test.actividad( 
-
-    id_actividad  integer unsigned auto_increment primary key, 
-
-    nombre varchar(50), 
-
-    descripcion varchar(200), 
-
-    aforo integer, 
-
-    precio_socio decimal(10,2), 
-
-    precio_no_socio decimal(10,2), 
-
-    fecha_ini date, 
-
-    fecha_fin date, 
-
-    deporte varchar(20), 
-
-    foreign key (instalacion) references instalacion(id_instalacion), 
-
-    foreign key (periodo_inscripcion) references periodo_inscripcion(id_periodo_inscripcion) 
-
-    ); 
-
-  
-
-INSERT INTO db_test.actividad(nombre, descripcion, aforo, precio_socio, precio_no_socio, fecha_ini, fecha_fin, deporte, instalaciÃ³n, periodo_inscripcion) VALUES 
+INSERT INTO actividades(nombre, descripcion, aforo, precio_socio, precio_no_socio, fecha_ini, fecha_fin, deporte, instalaciÃ³n, periodo_inscripcion) VALUES 
 
 ('Torneo Padel I', 'Primer torneo de padel.' , '32', '8.50', '12.00', '2022-2-24', '2022-3-3', 'padel', '1', '1'), 
 
 ('Torneo Tenis I', 'Primer torneo de tenis.' , '24', '10.00', '15.00', '2022-2-27', '2022-3-10', 'tenis', '2', '2'), 
 
 ('Torneo Futbol I', 'Primer torneo de futbol.' , '100', '6.50', '10.00', '2022-3-17', '2022-3-27', 'futbol', '3', '3'); 
-
- 
-
- 
-
-CREATE TABLE db_test.periodo_inscripcion( 
-
-    id_periodo_inscripcion integer unsigned auto_increment primary key, 
-
-    fecha_ini_socio date, 
-
-    fecha_fin_socio date, 
-
-    fecha_fin_no_socio date 
-
-); 
-
-  
-
- 
-
-INSERT INTO db_test.periodo_inscripcion(fecha_ini_socio, fecha_fin_socio, fecha_fin_no_socio) VALUES 
+INSERT INTO periodos_inscripcion(fecha_ini_socio, fecha_fin_socio, fecha_fin_no_socio) VALUES 
 
 ('2022-2-24', '2022-3-24', '2022-3-30'), 
 
 ('2022-2-27', '2022-3-30', '2022-4-30'), 
 
 ('2022-3-17', '2022-3-30', '2022-4-30'); 
-
- 
-
-CREATE TABLE db_test.inscripcion( 
-
-id_inscripcion integer unsigned auto_increment primary key, 
-
-    foreign key (persona) references socios(dni), 
-
-    foreign key (actividad) references actividad(id_actividad), 
-
-    fecha datetime 
-
-   ); 
-
-INSERT INTO db_test.inscripcion(persona, actividad, fecha) VALUES 
+INSERT INTO inscripciones(persona, actividad, fecha) VALUES 
 
 ('12345678C', 1, '2022-2-23 17:15:10'), 
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,99 @@
---Primero se deben borrar todas las tablas (de detalle a maestro) y lugo anyadirlas (de maestro a detalle)
---(en este caso en cada una de las aplicaciones (tkrun y descuento) se usa solo una tabla, por lo que no hace falta)
+DROP TABLE socios;
+DROP TABLE clientes;
+DROP TABLE instalaciones;
+DROP TABLE reservas;
+DROP TABLE periodos_inscripcion;
+DROP TABLE actividades;
+DROP TABLE inscripciones;
 
---Para giis.demo.tkrun:
-drop table Carreras;
-create table Carreras (id int primary key not null, inicio date not null, fin date not null, fecha date not null, descr varchar(32), check(inicio<=fin), check(fin<fecha));
+CREATE TABLE socios( 
 
+id_socio integer unsigned auto_increment primary key, 
+
+    nombre varchar(50), 
+
+    fecha_nacimiento date, 
+
+    dni varchar(9) 
+
+);
+CREATE TABLE clientes( 
+
+id_dni_cliente varchar(9) unique primary key, 
+
+    nombre varchar(50), 
+
+    fecha_nacimiento date 
+
+); 
+CREATE TABLE instalaciones( 
+
+    id_instalacion integer unsigned auto_increment primary key, 
+
+    nombre varchar(50), 
+
+    deporte varchar(20), 
+
+    precio decimal(10,2) 
+
+   );
+CREATE TABLE reservas( 
+
+id_reserva integer unsigned auto_increment primary key, 
+
+    foreign key (socio) references socios(id_socio), 
+
+    foreign key (instalacion) references instalaciones(id_instalacion), 
+
+    fecha datetime, 
+
+    fecha_reserva datetime 
+
+   ); 
+CREATE TABLE periodos_inscripcion( 
+
+    id_periodo_inscripcion integer unsigned auto_increment primary key, 
+
+    fecha_ini_socio date, 
+
+    fecha_fin_socio date, 
+
+    fecha_fin_no_socio date 
+
+); 
+CREATE TABLE actividades( 
+
+    id_actividad  integer unsigned auto_increment primary key, 
+
+    nombre varchar(50), 
+
+    descripcion varchar(200), 
+
+    aforo integer, 
+
+    precio_socio decimal(10,2), 
+
+    precio_no_socio decimal(10,2), 
+
+    fecha_ini date, 
+
+    fecha_fin date, 
+
+    deporte varchar(20), 
+
+    foreign key (instalacion) references instalaciones(id_instalacion) 
+
+    foreign key (periodo_inscripcion) references periodos_inscripcion(id_periodo_inscripcion) 
+
+    );
+CREATE TABLE inscripciones( 
+
+id_inscripcion integer unsigned auto_increment primary key, 
+
+    foreign key (persona) references socios(dni), 
+
+    foreign key (actividad) references actividades(id_actividad), 
+
+    fecha datetime 
+
+   ); 
