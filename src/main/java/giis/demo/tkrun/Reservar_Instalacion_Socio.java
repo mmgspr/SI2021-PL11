8d149b5dda391c081cefb5a33314cbce9d7c6763
David <David@DESKTOP-PVUS0LU>
2022-02-27 19:51:35 +0100

    Merge branch 'Trabajo' of https://github.com/uo275943/SI2021-PL11.git into Trabajo

--- src/main/java/giis/demo/tkrun/Reservar_Instalacion_Socio.java
+++ src/main/java/giis/demo/tkrun/Reservar_Instalacion_Socio.java
@@ -19,6 +19,7 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
 import java.util.Iterator;
 import java.util.List;
 import java.awt.event.ActionEvent;
@@ -28,6 +29,7 @@ public class Reservar_Instalacion_Socio {
 
 	private InstalacionesModel modelo = new InstalacionesModel();
 	private CosteModel modeloCoste = new CosteModel();
+	private ReservasModel modeloReservas = new ReservasModel();
 	private JFrame frmReservarInstalacin;
 	private JTextField textFieldCoste;
 	private SwingMain principal;
@@ -88,7 +90,7 @@ public class Reservar_Instalacion_Socio {
 		frmReservarInstalacin.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
-		JLabel LabelFechadeReserva = new JLabel("Fecha Inicio Reserva:");
+		JLabel LabelFechadeReserva = new JLabel("Fecha de la Reserva:");
 		LabelFechadeReserva.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		LabelFechadeReserva.setBounds(10, 10, 141, 27);
 		panel.add(LabelFechadeReserva);
@@ -101,7 +103,7 @@ public class Reservar_Instalacion_Socio {
 		textFieldCoste = new JTextField();
 		textFieldCoste.setEditable(false);
 		//textFieldCoste.setModel(new DefaultComboBoxModel(instalaciones));
-		textFieldCoste.setBounds(146, 166, 126, 19);
+		textFieldCoste.setBounds(161, 166, 93, 19);
 		panel.add(textFieldCoste);
 		textFieldCoste.setColumns(10);
 		
@@ -113,13 +115,13 @@ public class Reservar_Instalacion_Socio {
 		JCheckBox CheckBoxEstaLibre = new JCheckBox("Está libre");
 		CheckBoxEstaLibre.setEnabled(false);
 		CheckBoxEstaLibre.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		CheckBoxEstaLibre.setBounds(41, 92, 93, 21);
+		CheckBoxEstaLibre.setBounds(161, 108, 93, 21);
 		panel.add(CheckBoxEstaLibre);
 		
 		JCheckBox CheckBoxPuedesReservar = new JCheckBox("Puedes Reservar");
 		CheckBoxPuedesReservar.setEnabled(false);
 		CheckBoxPuedesReservar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		CheckBoxPuedesReservar.setBounds(164, 92, 152, 21);
+		CheckBoxPuedesReservar.setBounds(264, 108, 152, 21);
 		panel.add(CheckBoxPuedesReservar);
 		
 		JComboBox comboBoxMetodo = new JComboBox();
@@ -154,7 +156,6 @@ public class Reservar_Instalacion_Socio {
 		panel.add(LabelInstalacion);
 		
 		JComboBox comboBoxInstalaciones = new JComboBox();
-		comboBoxInstalaciones.setModel(new DefaultComboBoxModel(instalaciones));
 		comboBoxInstalaciones.setBounds(160, 135, 141, 21);
 		panel.add(comboBoxInstalaciones);
 		
@@ -167,9 +168,60 @@ public class Reservar_Instalacion_Socio {
 		LabelHoraIni.setBounds(10, 47, 113, 19);
 		panel.add(LabelHoraIni);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(158, 48, 85, 21);
-		panel.add(comboBox);
+		JComboBox comboBoxHoraIni = new JComboBox();
+		comboBoxHoraIni.setModel(new DefaultComboBoxModel(new String[] {"10:00:00", "11:00:00", "12:00:00", "13:00:00", "14:00:00", "15:00:00", "16:00:00", "17:00:00", "18:00:00", "19:00:00", "20:00:00", "21:00:00", "22:00:00"}));
+		comboBoxHoraIni.setBounds(158, 48, 85, 21);
+		panel.add(comboBoxHoraIni);
+		
+		JLabel LabelFin = new JLabel("Hora de Fin:");
+		LabelFin.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		LabelFin.setBounds(10, 76, 85, 17);
+		panel.add(LabelFin);
+		
+		JComboBox comboBoxHoraFin = new JComboBox();
+		comboBoxHoraFin.setModel(new DefaultComboBoxModel(new String[] {"10:00:00", "11:00:00", "12:00:00", "13:00:00", "14:00:00", "15:00:00", "16:00:00", "17:00:00", "18:00:00", "19:00:00", "20:00:00", "21:00:00", "22:00:00"}));
+		comboBoxHoraFin.setBounds(159, 79, 85, 21);
+		panel.add(comboBoxHoraFin);
+		
+		JButton ButtonComprobar = new JButton("Comprobar");
+		ButtonComprobar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//id_instalacion
+				String id;
+				
+				List<Object[]> lista=modelo.getIdInstalacion((String)comboBoxInstalaciones.getSelectedItem());
+				String[] nombre=new String[lista.size()];
+				Iterator<Object[]> iterador = lista.iterator();
+				
+				int i=0;
+				while(iterador.hasNext()) {
+					nombre[i]=iterador.next()[0].toString();
+					i++;
+				}
+				
+				id = nombre[0];
+				System.out.println(id);
+				
+				//fecha
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+				String date = sdf.format(dateChooserInicio.getDate());
+				String hora = (String)comboBoxHoraIni.getSelectedItem();
+				String diaHora = date+" "+hora;
+				System.out.println(diaHora);
+				if (modeloReservas.comprobarDisponibilidad(id, diaHora)) {
+					CheckBoxEstaLibre.setSelected(true);
+					
+				}
+				else System.out.println("Está ocupado");
+				
+				
+				
+				
+			}
+		});
+		ButtonComprobar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		ButtonComprobar.setBounds(10, 108, 113, 21);
+		panel.add(ButtonComprobar);
 	}
 
 
