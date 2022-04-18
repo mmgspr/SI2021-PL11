d61e0c819ddc204d5e2377a10185cea377a36174
danig <danig@DESKTOP-VUBHJ99>
2022-04-18 12:50:12 +0200

    creo que funciona

--- src/main/java/giis/demo/tkrun/GestionColas.java
+++ src/main/java/giis/demo/tkrun/GestionColas.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,33 +13,39 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class GestionColas {
-	
+	private static ArrayList<ArrayList<ArrayList<Integer>>> superLista ;
 	private static ArrayList<Integer> socios;
-	private static ArrayList<String> clientes;
+	private static ArrayList<Integer> clientes;
+	
 	private static FileOutputStream fos = null;
 	private static ObjectOutputStream oos= null;
 	private static FileInputStream fis= null;
 	private static ObjectInputStream ois = null;
-	private static FileOutputStream fosAux = null;
+	/*private static FileOutputStream fosAux = null;
 	private static ObjectOutputStream oosAux= null;
 	private static FileInputStream fisAux= null;
-	private static ObjectInputStream oisAux = null;
+	private static ObjectInputStream oisAux = null;*/
 	private static String ruta;
 	private static String rutaTmp;
     private static File file, tmp;
+    
+    
 	
-	public static int inicializa() {
+	/*public static int inicializa() {
 		//colas
 		socios = new ArrayList<Integer>();
 		clientes = new ArrayList<String>();
 		//fichero
 		try {
 			ruta = "src/main/resources/listas.txt";
-			ruta = "src/main/resources/tmp.txt";
+			rutaTmp = "src/main/resources/tmp.txt";
+			
 	        file = new File(ruta);
 	        tmp = new File(rutaTmp);
+	       
 	        // Si el archivo no existe es creado
 	        if (!file.exists()) {
 	            file.createNewFile();
@@ -46,40 +53,46 @@ public class GestionColas {
 	        if (!tmp.exists()) {
 	            tmp.createNewFile();
 	        }
-			//controladores
+	        			
+	        //controladores
 			fos = new FileOutputStream(file);
 	        oos = new ObjectOutputStream(fos); 
 	        fis = new FileInputStream(file);
 	        ois = new ObjectInputStream(fis);
-	        fosAux = new FileOutputStream(tmp);
-	        oosAux = new ObjectOutputStream(fosAux); 
-	        fisAux = new FileInputStream(tmp);
-	        oisAux = new ObjectInputStream(fisAux);
+	       
 		}catch(Exception e) {
 			System.out.println("Error al inicializar las colas o el fichero.");
 			return -1;
 		}
 		
 		return 0;
-	}
+	}*/
 	
 	
-	private static void serializa(ObjectOutputStream oosArg, ArrayList<Integer> socios,ArrayList<String> clientes, int idActividad) {
-//		try {
-//			 String ruta = "src/main/resources/listas";
-//	         String contenido = "Actividad: "+idActividad;
-//	         File file = new File(ruta);
-//	         // Si el archivo no existe es creado
-//	         if (!file.exists()) {
-//	             file.createNewFile();
-//	         }
-//	         FileWriter fw = new FileWriter(file);
-//	         BufferedWriter bw = new BufferedWriter(fw);
-//	         bw.write(contenido);
-//			    bw.close();
+	/*private static void serializa(ObjectOutputStream oosArg, ArrayList<Integer> socios,ArrayList<String> clientes, int idActividad) {
+		try {
+			 String ruta = "src/main/resources/listas.csv";
+	         String contenido = "Actividad: "+idActividad;
+	         File file = new File(ruta);
+	         // Si el archivo no existe es creado
+	         if (!file.exists()) {
+	             file.createNewFile();
+	         }
+	         FileWriter fw = new FileWriter(file);
+	         BufferedWriter bw = new BufferedWriter(fw);
+	         bw.write(contenido);
+	         for(int i = 0;i<socios.size();i++) {
+	        	 bw.write(socios.get(i));
+	         }
+	         for(int i = 0;i<clientes.size();i++) {
+	        	 bw.write(clientes.get(i));
+	         }
+			    bw.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	         
-//	         FileOutputStream fos = new FileOutputStream(file);
-//	         ObjectOutputStream oos = new ObjectOutputStream(fos);        
+	                
 			   
 		     try {
 				oosArg.writeInt(idActividad);
@@ -96,9 +109,9 @@ public class GestionColas {
 //	    } catch (Exception e1) {
 //	        e1.printStackTrace();
 //	    }
-	}
+	}*/
 	
-	private static ArrayList<?>[] desserializa(int idActividad) {
+	/*private static ArrayList<?>[] desserializa(int idActividad) {
 		ArrayList[] retorno = null;
 		int actividad=-1;
 		ArrayList<Integer> socios = null;
@@ -138,9 +151,9 @@ public class GestionColas {
 		}
 			
 		return retorno;
-	}
+	}*/
 	
-	public static void borrarFichero() {
+	/*public static void borrarFichero() {
 		try {
 			//cerrar los anteriores
 			oos.close();
@@ -156,16 +169,21 @@ public class GestionColas {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
+	}*/
 	
-	@SuppressWarnings("unchecked")
+	/*@SuppressWarnings("unchecked")
 	//esta funcion copia el fichero entero menos una actividad en concreto para poder modificarla
 	//se copia en un fichero temporal aparte
 	public static void copiarFichero(int idActividad) {
 		int actividad;
 		ArrayList<Integer> sociosAux;
 		ArrayList<String> clientesAux;
+			
 		try {
+			fosAux = new FileOutputStream(tmp);
+			oosAux = new ObjectOutputStream(fosAux); 
+	        fisAux = new FileInputStream(tmp);
+	        oisAux = new ObjectInputStream(fisAux);
 			while(true) {
 				//leer
 				actividad = oisAux.readInt();
@@ -176,28 +194,39 @@ public class GestionColas {
 					serializa(oosAux,sociosAux,clientesAux,actividad);
 				}
 			}
+			
 		}catch(EOFException e){
-			
+			try {
+				oosAux.close();
+				oisAux.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		}catch(Exception e) {
-			
+			System.out.println("Error al copiar el fichero.");
 		}
 		
-	}
+	}*/
 	
-	private static void eliminaActividad(int idActividad) {
+	/*private static void eliminaActividad(int idActividad) {
 		copiarFichero(idActividad);
 		borrarFichero();
 		try {
-			Files.copy(tmp.toPath(), file.toPath());
+			//Files.copy(tmp.toPath(), file.toPath(),StandardCopyOption.REPLACE_EXISTING);
+			int c;
+			while ((c = fis.read()) != -1) {
+                fosAux.write(c);
+            }
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
+	}*/
 	
-	@SuppressWarnings("unchecked")
+	/*@SuppressWarnings("unchecked")
 	public static int anadeSocio(int idSocio, int idActividad) {
-		ArrayList<?>[] colas = desserializa(idActividad);
+		//ArrayList<?>[] colas = desserializa(idActividad);
 		//no hay cola para esa actividad, luego hay que crearla
 		if(colas == null) {
 			socios = new ArrayList<Integer>();
@@ -237,15 +266,159 @@ public class GestionColas {
 		serializa(oos,socios,clientes,idActividad);
 		
 		return 0;
-	}
+	}*/
 	
-	public static void main(String[] args) {
+	/*public static void ver() {
+		ArrayList<Integer> s;
+		ArrayList<String> c;	
+        
+		System.out.println("Funcion ver para visualizar el fichero entero");
+		try {
+		while(true) {
+			System.out.println(ois.readInt());
+			s = (ArrayList<Integer>)ois.readObject();
+			c = (ArrayList<String>)ois.readObject();
+			for(int i = 0; i<s.size(); i++) System.out.println(s.get(i));
+			for(int i = 0; i<c.size(); i++) System.out.println(c.get(i));
+		}
+		}catch(EOFException e) {
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Acaba la funcion ver");
+		
+	}*/
+    
+    
+    
+    @SuppressWarnings("unchecked")
+	public static int inicializa() {
+    	try {
+    		ruta = "src/main/resources/listas.txt";
+        	File file = new File(ruta);
+    		
+            // Si el archivo no existe es creado
+            if (file.exists()) {
+            	fis = new FileInputStream(file);
+    	        ois = new ObjectInputStream(fis);
+            	superLista = (ArrayList<ArrayList<ArrayList<Integer>>>) ois.readObject();
+            	ois.close();
+    			fis.close();
+            }
+            else {
+            	superLista = new ArrayList<ArrayList<ArrayList<Integer>>>();
+            }
+    	}catch(Exception e) {
+    		e.printStackTrace();
+    	}
+    	
+    	return 0;
+    }
+    
+    public static int serializa() {
+    	try {
+    		ruta = "src/main/resources/listas.txt";
+        	File file = new File(ruta);
+        	fos = new FileOutputStream(file);
+	        oos = new ObjectOutputStream(fos); 
+            // Si el archivo no existe es creado
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            oos.writeObject(superLista);
+            oos.close();
+			fos.close();
+    	}catch(Exception e) {
+    		e.printStackTrace();
+    	}
+    	
+    	return 0;
+    }
+    
+    public static int anadeSocio(int idSocio, int idActividad) {
+    	boolean encontrado= false;
+    	ArrayList<ArrayList<Integer>> anadir;
+    	ArrayList<Integer> anadir2;
+    	ArrayList<Integer> anadir3;
+    	ArrayList<Integer> anadir4;
+    	for(int i = 0;i<superLista.size();i++) {
+    		if(superLista.get(i).get(0).get(0) == idActividad) {
+    			superLista.get(i).get(1).add(idSocio);
+    			encontrado = true;
+    			break;
+    		}
+    	}
+    	if (!encontrado) {
+    		anadir2 = new ArrayList<Integer>();
+    		anadir2.add(idActividad);
+    		anadir3 = new ArrayList<Integer>();
+    		anadir3.add(idSocio);
+    		anadir4 = new ArrayList<Integer>();
+    		anadir = new ArrayList<ArrayList<Integer>>();
+    		anadir.add(anadir2);
+    		anadir.add(anadir3);
+    		anadir.add(anadir4);
+    		superLista.add(anadir);
+    	}
+    	  	
+        
+    	return 0;
+    }
+    
+    public static int anadeCliente(int tlf, int idActividad) {
+    	boolean encontrado= false;
+    	ArrayList<ArrayList<Integer>> anadir;
+    	ArrayList<Integer> anadir2;
+    	ArrayList<Integer> anadir3;
+    	ArrayList<Integer> anadir4;
+    	for(int i = 0;i<superLista.size();i++) {
+    		if(superLista.get(i).get(0).get(0) == idActividad) {
+    			superLista.get(i).get(2).add(tlf);
+    			encontrado = true;
+    			break;
+    		}
+    	}
+    	if (!encontrado) {
+    		anadir2 = new ArrayList<Integer>();
+    		anadir2.add(idActividad);
+    		anadir3 = new ArrayList<Integer>();
+    		anadir4 = new ArrayList<Integer>();
+    		anadir4.add(tlf);
+    		anadir = new ArrayList<ArrayList<Integer>>();
+    		anadir.add(anadir2);
+    		anadir.add(anadir3);
+    		anadir.add(anadir4);
+    		superLista.add(anadir);
+    	}
+    	
+        
+    	return 0;
+    }
+    
+    public static void ver() {
+    	for (ArrayList<ArrayList<Integer>> i : superLista) {
+    		System.out.println("Actividad " + i.get(0).get(0));
+    		System.out.print("Socios: ");
+    		for(Integer j : i.get(1)) {
+    			System.out.print(j + ", ");
+    		}
+    		System.out.print("Clientes: ");
+    		for(Integer j : i.get(2)) {
+    			System.out.print(j + ", ");
+    		}
+    		System.out.println("");
+    	}
+    }
+    
+	public static void main(String[] args) throws IOException {
 		
 //        FileWriter fw = new FileWriter(file);
 //        BufferedWriter bw = new BufferedWriter(fw);
 //        bw.write(contenido);
 //		    bw.close();		
-		
+		/*inicializa();
 		ArrayList<Integer> socios = new ArrayList<Integer>();
 		socios.add(1);
 		socios.add(4);
@@ -264,10 +437,12 @@ public class GestionColas {
 		
 		serializa(oos, socios2, clientes2, 5);
 		serializa(oos, socios, clientes, 3);
-		
 		anadeSocio(10,5);
+		
 		anadeCliente("603441826",3);
+		
 		anadeSocio(2,7);
+		ver();
 		
 		System.out.println("hecho");
 		
@@ -280,7 +455,37 @@ public class GestionColas {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
+//		ruta = "src/main/resources/listas.txt";
+//		File file = new File(ruta);
+//        // Si el archivo no existe es creado
+//        if (!file.exists()) {
+//            file.createNewFile();
+//        }
+//		try {
+//			fos = new FileOutputStream(file);
+//	        oos = new ObjectOutputStream(fos); 
+//	        fis = new FileInputStream(file);
+//	        ois = new ObjectInputStream(fis);
+//		}catch(Exception e) {
+//			System.out.println("tus muertos");
+//		}
+		inicializa();
+		//superLista = new ArrayList<ArrayList<ArrayList<Integer>>>();
+		/*anadeSocio(7,4);
+		anadeSocio(7,3);
+		anadeSocio(7,6);
+		anadeCliente(673548725,4);
+		anadeCliente(603441826,4);*/
+		anadeCliente(673548725,6);
+		anadeCliente(673548725,8);
+		/*System.out.println(superLista.get(0).get(0).get(0));
+		System.out.println(superLista.get(0).get(1).get(0) +" "+ superLista.get(0).get(1).get(1));
+		System.out.println(superLista.get(0).get(2).get(0));
+		System.out.println(superLista.get(0).get(2).get(1));*/
+		ver();
+		serializa();
+		
 		 
 	}
 
